6d7806e2ccd5cb1ee46a02ec9dc3e6d17b5e8522
SohamGlobal <sohamglobal@users.noreply.github.com>
2025-02-18 09:08:14 +0530

    Add files via upload

--- ObjectOrientedCode/Programs/MyShow.java
+++ ObjectOrientedCode/Programs/MyShow.java
@@ -4,10 +4,16 @@ import com.sharayu.classes.Film;
 
 public class MyShow {
 	public static void main(String[] args) {
-		Film f=new Film("pk",2014,"comedy","hindi",8.7);
+		Film f=new Film("matrix",1999,"sci-fi","english",8.7);
+		//f.filmname="the matrix";
+		f.setFilmname("the matrix");
+		f.setGenre("action");
+		
 		System.out.println(f.getFilmname());
-		f.setFilmname("sholay");
-		System.out.println(f.getFilmname());
+		System.out.println(f.getGenre());
+		
+		System.out.println(f);
+		
 	}
 
 }
