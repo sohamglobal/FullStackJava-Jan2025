f4a157e7860c7351acb7649c27881f5b628a2156
SohamGlobal <sohamglobal@users.noreply.github.com>
2025-02-20 09:05:07 +0530

    Add files via upload

--- ObjectOrientedCode/Programs/TryInheritance.java
+++ ObjectOrientedCode/Programs/TryInheritance.java
@@ -1,13 +1,19 @@
 package com.praffull.programs;
 
+import com.sharayu.classes.Three;
 import com.sharayu.classes.Two;
 
 public class TryInheritance {
 	public static void main(String[] args) {
-		Two obj=new Two();
-		obj.calcSquare(9);
-		//function of class One
-		obj.calcSum(12,34);
+		Two obj1=new Two();
+		obj1.calcSquare(25);
+		obj1.calcSum(55, 77);
+				
+		Three obj2=new Three();
+		obj2.showDiscount(12500.00); //Three
+		obj2.calcSum(23,89); //One
+		//obj2.calcSquare(49); //Two
+		
 	}
 
 }
