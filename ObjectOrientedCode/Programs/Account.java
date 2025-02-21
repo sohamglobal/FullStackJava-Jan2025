9da691294138fcffb07137a87d1b66d0d7e48897
SohamGlobal <sohamglobal@users.noreply.github.com>
2025-02-21 09:06:40 +0530

    Add files via upload

---
+++ ObjectOrientedCode/Programs/Account.java
@@ -0,0 +1,11 @@
+package com.praffull.programs;
+
+import com.sharayu.classes.Banking;
+
+public class Account {
+	public static void main(String[] args) {
+		Banking b=new Banking();
+		b.calcInterest("saving", 45700.00);
+	}
+
+}
---
+++ ObjectOrientedCode/Programs/ConstructorBehavior.java
@@ -0,0 +1,10 @@
+package com.praffull.programs;
+
+import com.sharayu.classes.Soham;
+
+public class ConstructorBehavior {
+	public static void main(String[] args) {
+		Soham obj=new Soham();
+	}
+
+}
