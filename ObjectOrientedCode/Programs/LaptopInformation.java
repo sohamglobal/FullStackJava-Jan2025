144772a3ed09cda6b58f264156370ffce8096dfe
SohamGlobal <sohamglobal@users.noreply.github.com>
2025-02-22 09:07:54 +0530

    Add files via upload

---
+++ ObjectOrientedCode/Programs/LaptopInformation.java
@@ -0,0 +1,13 @@
+package com.praffull.programs;
+
+import com.sharayu.classes.Dell;
+
+public class LaptopInformation {
+	public static void main(String[] args) {
+		Dell d=new Dell();
+		d.welcome();
+		d.showSpecs();
+		d.calcDiscount(37000.00);
+	}
+
+}
