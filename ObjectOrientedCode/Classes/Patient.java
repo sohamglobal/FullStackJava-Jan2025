61c45d1333a89283f8b9bc8a3e00f77bfc88a937
SohamGlobal <sohamglobal@users.noreply.github.com>
2025-02-25 09:05:35 +0530

    Add files via upload

---
+++ ObjectOrientedCode/Classes/Patient.java
@@ -0,0 +1,16 @@
+package com.sharayu.classes;
+
+public class Patient {
+	private static String name;
+	
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	
+	public static void showName()
+	{
+		System.out.println("Patient name is "+name);
+	}
+
+}
