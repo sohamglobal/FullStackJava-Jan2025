7046f08bf3d43720ddf4c1295db8f295ffb6a1fa
SohamGlobal <sohamglobal@users.noreply.github.com>
2025-03-11 09:04:45 +0530

    Add files via upload

---
+++ CollectionsFramework/src/com/sharayu/programs/PriorityQueueDemo.java
@@ -0,0 +1,20 @@
+package com.sharayu.programs;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class PriorityQueueDemo {
+	public static void main(String[] args) {
+		//PriorityQueue<Integer> nums=new PriorityQueue<Integer>();
+		PriorityQueue<Integer> nums=new PriorityQueue<Integer>(Comparator.reverseOrder());
+		nums.add(23);
+		nums.add(75);
+		nums.add(9);
+		nums.add(13);
+		nums.add(45);
+		//nums.add(null); not allowed
+		nums.add(75);
+		System.out.println(nums);
+	}
+
+}
