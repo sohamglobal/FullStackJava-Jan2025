6b58d0c25ef9a30a113cc20d4b183c3656d638b5
SohamGlobal <sohamglobal@users.noreply.github.com>
2025-05-13 09:13:54 +0530

    Add files via upload

---
+++ HiberCoreBanking/src/com/sharayu/programs/NewAccount.java
@@ -0,0 +1,27 @@
+package com.sharayu.programs;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.sohamglobal.entities.Account;
+
+public class NewAccount {
+	public static void main(String[] args) {
+		Account obj=new Account();
+		obj.setAccno(1053);
+		obj.setAccnm("owee kale");
+		obj.setAcctype("fixed");
+		obj.setBalance(35600);
+		
+		Configuration cfg=new Configuration().configure();
+		SessionFactory sf=cfg.addAnnotatedClass(Account.class).buildSessionFactory();
+		Session ses=sf.getCurrentSession();
+		ses.beginTransaction();
+		
+		ses.persist(obj);
+		ses.getTransaction().commit();
+		System.out.println("New Account Created");
+	}
+
+}
